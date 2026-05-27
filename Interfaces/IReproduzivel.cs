5a35295b66f19d9158afe52de62ef1c3811d97ff
Massarelli <massarelli_direito@hotmail.com>
2026-05-27 20:07:24 -0300

    Acertos

--- Interfaces/IReproduzivel.cs
+++ Interfaces/IReproduzivel.cs
@@ -2,8 +2,8 @@ namespace POO_Aula08.Interfaces;
 
 public interface IReproduzivel
 {
-    string Titulo { get; } // 👈 Agora toda mídia precisa expor seu título
-    bool EmReproducao { get; set; } // 👈 O contrato agora exige saber o status
+    string Titulo { get; } // Agora toda mídia precisa expor seu título
+    bool EmReproducao { get; set; } // O contrato agora exige saber o status
     void Play();
     void Pause();
 }
